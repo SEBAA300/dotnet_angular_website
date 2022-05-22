c51705330644692def2537aa3ac6d4538ad00af0
SEBAA300 <sebaa.13.06@gmail.com>
2022-05-22 18:06:34 +0200

    added new page and tested localstorage

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  key: string="User";
+  username: string="";
+  onclick(newName: string){ 
+    this.username = newName; 
+    localStorage.setItem(this.key, newName);
+  }
+
+  getName(){
+    return localStorage.getItem("User");
+  }
 }
